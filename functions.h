9aea9a768bb09be9d2a0a0156c0861fe0e5d4226
kcmando1 <marti.soucheiron@hotmail.com>
2019-03-16 11:40:58 +0100

    updated file structure for .h working in the same directory

---
+++ functions.h
@@ -0,0 +1,12 @@
+//insertion functions.h file
+#ifndef _functions_h
+#define _functions_h
+
+typedef struct {
+	int a;
+
+} asdf;
+  
+int clientStrRead(char *r, int *cr, float *n);
+int serverStrRead (char *r, int *v, int *t, int *m);
+#endif
