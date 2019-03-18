0f4bd4d89908fb8641df54fe4347b4ddee9778fe
alumne <alumne@lae2008.(none)>
2019-03-18 19:21:02 +0100

    solved .h feature

--- functions.h
+++ functions.h
@@ -1,6 +1,6 @@
 //insertion functions.h file
-//#ifndef FUNCTIONS_H
-//#define FUNCTIONS_H
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,4 +8,4 @@
 void prova();
 int clientStrRead(char *r, int *cr, float *n);
 int serverStrRead (char *r, int *v, int *t, int *m);
-//#endif
+#endif
