0f1d0ddd55285c5b2f8bd5ed3058abc06f0cc9f7
alumne <alumne@lae2008.(none)>
2019-03-20 19:04:33 +0100

    enviament millorat i client amb ip cambiable

--- toni/enviament_dades.c
+++ toni/enviament_dades.c
@@ -5,18 +5,16 @@
 #define cls "clear"
 
 
-
-void main()
+//void clientMenuStr(char* str)
+int main(/*char* buffer*/)
 {
-	int ref = -1;
-	char misatgefinal[20];
+	char* buffer;
 	char misatge[20]="";
-	int i = 0;int j = 0;
 	char temps[2];
-	char tempsinf[2];
 	char mostres [5];
-	int T, T1, M;
-	int aux, aux1, aux2;
+	int flag=0;
+	char sta,tmp[2],tmp1;
+	int aux, aux1, aux2=-1;
 	
 	printf("Posar en marxa o aturar = M\n");
 	printf("Demanar mitjana aritmetica= U\n");
@@ -29,14 +27,14 @@ void main()
 
 
 	
-	misatgefinal[i]='{'; // i = 0
+	
 	scanf("%c",misatge);
-	i++;	// i = 1
-	misatgefinal[i]=toupper(misatge[j]); //j = 0
+	
+	
 
 
 
-	switch(misatge[j]){
+	switch(misatge[0]){
 		case 'M':;
 		case 'm':
 			system(cls);
@@ -46,186 +44,150 @@ void main()
 			aux=(int) misatge[0]; //->nyapa de pasar de ascii a valor del caracter de la taula ascii en int
 			//printf("\n valor m1 %c",aux);
 			if(aux==48||aux==49){//48 ->0 49 ->
-					//printf("\n caca \n");
-				
-				
-				i++;//i = 2
-				misatgefinal[i]=misatge[j];	
-				i++;j++; // i = 3	
+				sta=misatge[0];
+			}else{
+				printf("error");
+				flag=1;
+			}		
+//------------------------------------------------------------------------				
 				system(cls);
 				printf("Introduir un temps de 1 a 20 segons:\n");
 				scanf("%s",temps);
-				printf("/n stringa %s /n",temps);
+				//printf("/n stringa %s /n",temps);
 				aux=(int) temps[0];
 				aux1=(int) temps[1];
 				
-				printf("/n int1=%d /n",aux);
-				printf("/n int2=%d /n",aux1);
-				switch(aux){
-					case 48:
-						if (aux1>48 &&aux1<=57){
-							//go
-							printf("\n caca1 \n");
-						}else{
-							//error
-							printf("\n culo 1 \n");
-						};
-					break;
-					
-					case 49:
-						if (aux1>=48 && aux1<=57){
-							//go
-							printf("\n caca2 \n");
-						}else{
-							//error
-							printf("\n culo 2 \n");
-						};
-					break;
-					
-					case 50:
-						if (aux1==48){
-							//go
-							printf("\n caca 3 \n");
-						}else{
-							//error
-							printf("\n culo 3 \n");
-						};
-					break;
-					default:
-						//error
-					
-				}
-					
+			
 				
-				T = atoi(temps[0]);
-				printf("/n T %d/n",T);
-				if(T<=9){
-					if(T<0){
-						printf("error");
-						ref = -1;
-						break;
-					}
-					if(T==00){
-						printf("error");
-						ref = -1;
-						break;
+				if (aux1=='\0'){
+					aux2=atoi(temps);
+					if((aux2 != 0)&&(0<aux2 && aux2<10)){
+						tmp[0]='0';
+						tmp[1]=temps;
 					}else{
-						tempsinf[0]='0';
-						tempsinf[1]=temps[0];
-						temps[0]='\0';
-						strcpy(temps,tempsinf);
-						misatgefinal[i]=temps[0];
-						i++; // i = 4
-						misatgefinal[i]=temps[1];
-						i++; // i = 5
+						//error
+						printf("error");
+						flag=1;
+					}
+				}else{
+					switch(aux){
+						case 48:
+							if (aux1>48 &&aux1<=57){
+								//go
+								tmp[0]=aux;
+								tmp[1]=aux1;
+								
+								
+							}else{
+								//error
+								printf("error");
+								flag=1;
+								printf("\n entrada de dades incorrecta \n");
+								break;
+							};
+							break;
+						
+						case 49:
+							if (aux1>=48 && aux1<=57){
+								//go
+								tmp[0]=aux;
+								tmp[1]=aux1;
+								
+								
+							}else{
+								//error
+								printf("error");
+								flag=1;
+								printf("\n entrada de dades incorrecta  2 \n");
+							};
+							break;
+						
+						case 50:
+							if (aux1==48){
+								//go
+								tmp[0]=aux;
+								tmp[1]=aux1;
+								
+							}else{
+								//error
+								printf("error");
+								flag=1;
+								printf("\n entrada de dades incorrecta  3 \n");
+							};
+							break;				
+						}
+				}	
+				printf("/n resultat t %s/n");	
+//------------------------------------------------------------------------				
 						system(cls);
 						printf("Introduir numero de mostres (0 a 9):\n");
 						scanf("%s",mostres);
-						T1 = atoi(mostres);
-						if(0<T1 && T1<10){
-						misatgefinal[i]=mostres[0];
-						i++; // i = 6
+						aux2 = atoi(mostres);
+						if(0<aux2 && aux2<10){
+							tmp1=aux2;
 						}else{
 							printf("error");
 							break;
 						}
-						misatgefinal[i]='}';
-						ref = 1;
-					}
-				}else{
-					if(T<0){
-						printf("error");
-						ref = -1;
-						break;
-					}
-					if(T>20){
-						printf("error");
-						ref = -1;
-						break;
-					}else{
-						misatgefinal[i]=temps[0];
-						i++; // i = 4
-						misatgefinal[i]=temps[1];
-						i++; // i = 5
-						system(cls);
-						printf("Introduir numero de mostres (0 a 9):\n");
-						scanf("%s",mostres);
-						T1 = atoi(mostres);
-						if(0<=T1 && T1<10){
-							misatgefinal[i]=mostres[0];
-							i++; // i = 6
-							misatgefinal[i]='}';
-							ref = 1;
-						}else{
-							printf("error");
-						break;
+//----------------------------------------------------------------------									
+						if(flag==0){
+							buffer[0]='{';
+							buffer[1]='M';
+							strcat(buffer,sta);
+							strcat(buffer,tmp);
+							strcat(buffer,tmp1);
+							buffer[5]='}';
+							buffer[6]='\0';
 						}		
-					}							
-				}
-			printf("Misatge=%s",misatgefinal);
-			}else{
-				printf("error");
-				break;
-			}
-			
 			break;
-			
+//----------------------------------------------------------------------			
 		case 'U':;
 		case 'u':
 			system(cls);
 			printf("Es vol rebre mitjana aritmètica\n");
-			ref = 2;
-			i++;
-			misatgefinal[i]='}';
+			
+			
 
-			printf("Misatge=%s",misatgefinal);
+			printf("Misatge=%s",buffer);
 			break;
-		
+//----------------------------------------------------------------------					
 		case'X':;
 		case'x':
 			system(cls);
 			printf("Es vol rebre el valor màxim\n");
-			ref = 3;
-			i++;
-			misatgefinal[i]='}';
 			
-			printf("Misatge=%s",misatgefinal);
+					
+			printf("Misatge=%s",buffer);
 			break;			
-		
+//----------------------------------------------------------------------					
 		case 'Y':;
 		case 'y':
 			system(cls);
 			printf("Es vol rebre el valor minim\n");
-			ref = 4;
-			i++;
-			misatgefinal[i]='}';
 			
-			printf("Misatge=%s",misatgefinal);
+			buffer=			
+			printf("Misatge=%s",buffer);
 			break;	
-		
+//----------------------------------------------------------------------					
 		case 'R':;
 		case 'r':
 			system(cls);
 			printf("Es vol rebre el valor maxim i minim\n");
-			i++;
-			misatgefinal[i]='}';
-			ref = 5;
 			
-			printf("Misatge=%s",misatgefinal);
+						
+			printf("Misatge=%s",buffer);
 			break;				
-		
+//----------------------------------------------------------------------				
 		case 'B':;
 		case 'b':
 			system(cls);
 			printf("Es vol rebre el valor de mostres guardades\n");
-			i++;
-			misatgefinal[i]='}';
-			ref = 6;
 			
-			printf("Misatge=%s",misatgefinal);
+					
+			printf("Misatge=%s",buffer);
 			break;					
-	
-} 
-return 0;
+//----------------------------------------------------------------------				
+	} 
+	return flag;
 }
 
